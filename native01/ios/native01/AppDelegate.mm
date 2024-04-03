f4429e70592ab06b9bb0930130abaeb2823b9024
seung-heee <tmdgmldi2595@naver.com>
2024-04-03 16:31:35 +0900

    React Native 카카오 로그인 iOS

---
+++ native01/ios/native01/AppDelegate.mm
@@ -0,0 +1,42 @@
+#import "AppDelegate.h"
+
+#import <React/RCTBundleURLProvider.h>
+#import <RNKakaoLogins.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)app
+     openURL:(NSURL *)url
+     options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+ if([RNKakaoLogins isKakaoTalkLoginUrl:url]) {
+    return [RNKakaoLogins handleOpenUrl: url];
+ }
+
+ return NO;
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  self.moduleName = @"native01";
+  // You can add your custom initial props in the dictionary below.
+  // They will be passed down to the ViewController used by React Native.
+  self.initialProps = @{};
+
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+  return [self getBundleURL];
+}
+
+- (NSURL *)getBundleURL
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
+@end
--- test_socialLogin/ios/SwiftBridge.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SwiftBridge.swift
-//  test_socialLogin
-//
-//  Created by 노승희 on 3/31/24.
-//
-
-import Foundation
