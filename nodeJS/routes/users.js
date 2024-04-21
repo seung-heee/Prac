31d28913afa38ada40a7dcb50ac0747543559c6b
seung-heee <tmdgmldi2595@naver.com>
2024-04-22 01:46:20 +0900

    Refactor: user 코드정리

--- nodeJS/routes/users.js
+++ nodeJS/routes/users.js
@@ -1,67 +1,26 @@
-// express 모듈 셋팅
 const express = require('express')
-const router = express.Router()
-router.use(express.json()) // http 외 모듈 'json' 사용
 const conn = require('../mysql')
-
-// Using placeholders
-// conn.query(
-//   'SELECT * FROM `users`',
-//   function (err, results, fields) {
-//     console.log(results);
-//     results.map((item)=>{
-//       console.log('item')
-
-//       // console.log('key : ', item.i)
-//       // console.log('title : ', item.title)
-//       // console.log('sub_title : ', item.sub_title)
-//       // console.log('img : ', item.img)
-//     })
-//   }
-// );
-
-// db
-let db = new Map()
-var id = 1 // 하나의 객체를 유니크하게 구별하기 위함.
-
-// 객체가 비었는지 확인하는 함수
-function isExisted(obj) {
-  if (Object.keys(obj).length) {
-    return true
-  } else {
-    return false
-  }
-}
+const router = express.Router()
+router.use(express.json())
 
 // 로그인
 router.post('/signin', (req, res) => {
-  var loginUser = {}
-  const { userId, password } = req.body
+  const { email, password } = req.body
 
-  // userId가 db에 저장된 회원인지 확인
-  db.forEach((user, idx)=>{ // data, idx, totalData
-    if (user.userId === userId) { // 요청데이터 userId와 db에 있는 userId가 같은지 확인
-      loginUser = user 
+  let SQL = `SELECT * FROM user WHERE email = ?`
+  conn.query(SQL, email,
+    function (err, results) {
+      var loginUser = results[0]
+      if (loginUser && loginUser.password == password) 
+          res.status(200).json({
+            message: `${loginUser.name}님 로그인 되었습니다.`
+          })
+      else
+        res.status(404).json({
+          message: "이메일 또는 비밀번호가 틀렸습니다."
+        })
     }
-  })
-  
-  // userId 값을 못 찾았으면
-  if(isExisted(loginUser)) {
-    // pwd도 맞는지 비교
-    if (loginUser.password === password) {
-      res.status(200).json({
-        message: `${loginUser.name}님 로그인 되었습니다.`
-      })
-    } else {
-      res.status(400).json({
-        message: `비밀번호가 틀렸습니다.`
-      })
-    }
-  } else {
-    res.status(404).json({
-      message: "회원 정보가 없습니다."
-    })
-  }
+  );
 })
 
 // 회원가입
@@ -71,54 +30,39 @@ router.post('/signup', (req, res) => {
       message: '입력 값을 다시 확인해주세요.'
     })
   } else {
-    const userId = req.body.userId
-    db.set(userId, req.body)
+    const {email, name, password, contact} = req.body
 
-    res.status(201).json({
-      message: `${db.get(userId).name}님 환영합니다.`
-    })
+    let SQL = `INSERT INTO user (email, name, password, contact) VALUES (?, ?, ?, ?)`
+    let values = [email, name, password, contact]
+    conn.query(SQL, values,
+      function (err, results) {
+        res.status(201).json(results)
+      }
+    )
   }
 })
 
-// 회원 개별 조회
 router
   .route('/users')
+  // 회원 개별 조회
   .get((req, res) => {
   let {email} = req.body
-  // const id = parseInt(req.params.id)
-  // const user = db.get(userId)
 
-  console.log(email)
-  conn.query(
-    `SELECT * FROM user Where email = ?`, email,
-    function (err, results, fields) {
-      // if (results.length)
-        res.status(200).json(results)
-        console.log(results)
-      // else {
-      //   res.status(404).json({
-      //     message: "회원 정보가 없습니다."
-      //   })
-      // }
-    }
-  );
-  })
+  let SQL = `SELECT * FROM user WHERE email = ?`
+  conn.query(SQL, email,
+    function (err, results) {
+      res.status(200).json(results)
+      }
+  )})
+  // 회원 개별 탈퇴
   .delete((req, res) => {
-    let { userId } = req.body
-    // const id = parseInt(req.params.id)
-    const user = db.get(userId)
+    const {email} = req.body
 
-    if (user) {
-      db.delete(userId)
-      
-      res.status(200).json({
-        message: `${user.name}님, 다음에 또 뵙겠습니다.`
-      })
-    } else {
-      res.status(404).json({
-        message: "회원 정보가 없습니다."
-      })
-    }
-  })
+    let SQL = `DELETE FROM user WHERE email = ?`
+    conn.query(SQL, email,
+      function (err, results, fields) {
+        res.status(200).json(results)
+      }
+  )})
 
 module.exports = router
